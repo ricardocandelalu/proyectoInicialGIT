07eb45355901953d3939fa79096913983ca00dbb
ricardocandelalu <57441938+ricardocandelalu@users.noreply.github.com>
2019-11-06 12:59:04 +0100

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,7 @@
 // El progarma "Hola, Mundo!" en Java	
 public class HolaMundo  {	
 public static void main (String[] args)  {	
-System.out.println ("Hola Mundo") ;
+System.out.println ("Hola GIT") ;
 }
 }
 
